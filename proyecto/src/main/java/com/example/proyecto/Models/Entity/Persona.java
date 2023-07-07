1d48bc2f7a89bb7417cd72a8a5dc527b6a5dade4
marko4510 <marcocuellar7293@gmail.com>
2023-07-07 11:02:06 -0400

    asdasd

---
+++ proyecto/src/main/java/com/example/proyecto/Models/Entity/Persona.java
@@ -0,0 +1,51 @@
+package com.example.proyecto.Models.Entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "persona")
+@Getter
+@Setter
+public class Persona implements Serializable{
+       private static final long serialVersionUID = 2629195288020321924L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_persona;
+    
+    private String estado;
+    private String tipoSexo;
+    private String nombres;
+    private String paterno;
+    private String materno;
+    private String ci;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date fecNacimiento;
+    
+    private String direccion;
+    private String telefono;
+    private String email;
+
+     @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "persona", fetch = FetchType.LAZY)
+    private List<Usuario> usuarios;
+    
+}
