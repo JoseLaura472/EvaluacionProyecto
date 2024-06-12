8968727801d84b312930d2ee8778d5ae5f31b2b6
abrahan24 <asaraviacoca@gmail.com>
2024-06-12 11:12:22 -0400

    UPDATE

--- proyecto/src/main/java/com/example/proyecto/Controllers/EvaluacionControllers/EvaluacionController.java
+++ proyecto/src/main/java/com/example/proyecto/Controllers/EvaluacionControllers/EvaluacionController.java
@@ -23,6 +23,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.proyecto.Models.Entity.Proyecto;
 import com.example.proyecto.Models.Entity.Usuario;
+import com.example.proyecto.Models.Dao.IEvaluacionDao;
+import com.example.proyecto.Models.Dao.IJuradoDao;
 import com.example.proyecto.Models.Entity.Criterio;
 import com.example.proyecto.Models.Entity.Evaluacion;
 import com.example.proyecto.Models.Entity.Jurado;
@@ -46,6 +48,8 @@ public class EvaluacionController {
     @Autowired
     private IJuradoService juradoService;
 
+    @Autowired
+    private IEvaluacionDao evaluacionDao;
     // FUNCION PARA LA VISUALIZACION DE REGISTRO DE MNACIONALIDAD
     @RequestMapping(value = "/ProyectosEvaluacionR", method = RequestMethod.GET) // Pagina principal
     public String EvaluacionR(HttpServletRequest request, Model model) {
@@ -85,101 +89,29 @@ public class EvaluacionController {
 
     // Boton para Editar Documentos
     @RequestMapping(value = "/form-evaluacion/{id_proyecto}")
-    public String editar_proyecto(@PathVariable("id_proyecto") Long id_proyecto, Model model) {
-        Evaluacion evaluacion = new Evaluacion();
+    public String editar_proyecto(@PathVariable("id_proyecto") Long id_proyecto, Model model, HttpSession session, HttpServletRequest request) {
+
+        if (request.getSession().getAttribute("usuario") != null) {
+            Evaluacion evaluacion = new Evaluacion();
         Proyecto proyecto = proyectoService.findOne(id_proyecto);
-        Long id_criterio1 = (long) 1;
-        Long id_criterio2 = (long) 2;
-        Long id_criterio3 = (long) 3;
-        Long id_criterio4 = (long) 4;
-        Long id_criterio5 = (long) 5;
-        Long id_criterio6 = (long) 6;
-        Long id_criterio7 = (long) 7;
-        Long id_criterio8 = (long) 8;
-        Long id_criterio9 = (long) 9;
-        Long id_criterio10 = (long) 10;
-        Long id_criterio11 = (long) 11;
-        Long id_criterio12 = (long) 12;
-        Long id_criterio13 = (long) 13;
-        Long id_criterio14 = (long) 14;
-        Long id_criterio15 = (long) 15;
-        Long id_criterio16 = (long) 16;
-        Long id_criterio17 = (long) 17;
-        Long id_criterio18 = (long) 18;
-        Long id_criterio19 = (long) 19;
-        Long id_criterio20 = (long) 20;
-        Long id_criterio21 = (long) 21;
-        Long id_criterio22 = (long) 22;
-        Long id_criterio23 = (long) 23;
-        Long id_criterio24 = (long) 24;
-        Long id_criterio25 = (long) 25;
-        Long id_criterio26 = (long) 26;
-        Long id_criterio27 = (long) 27;
-        Long id_criterio28 = (long) 28;
-        Long id_criterio29 = (long) 29;
-        Long id_criterio30 = (long) 30;
-        Long id_criterio31 = (long) 31;
-        Long id_criterio32 = (long) 32;
-        Long id_criterio33 = (long) 33;
-        Long id_criterio34 = (long) 34;
-        Long id_criterio35 = (long) 35;
-        Long id_criterio36 = (long) 36;
-        Long id_criterio37 = (long) 37;
-        Long id_criterio38 = (long) 38;
-        Long id_criterio39 = (long) 39;
-        Long id_criterio40 = (long) 40;
-        Long id_criterio41 = (long) 41;
-        Long id_criterio42 = (long) 42;
-        
         
         model.addAttribute("proyecto", proyecto);
         model.addAttribute("evaluacion", evaluacion);
-
-        model.addAttribute("criterio1", criterioService.findOne(id_criterio1));
-        model.addAttribute("criterio2", criterioService.findOne(id_criterio2));
-        model.addAttribute("criterio3", criterioService.findOne(id_criterio3));
-        model.addAttribute("criterio4", criterioService.findOne(id_criterio4));
-        model.addAttribute("criterio5", criterioService.findOne(id_criterio5));
-        model.addAttribute("criterio6", criterioService.findOne(id_criterio6));
-        model.addAttribute("criterio7", criterioService.findOne(id_criterio7));
-        model.addAttribute("criterio8", criterioService.findOne(id_criterio8));
-        model.addAttribute("criterio9", criterioService.findOne(id_criterio9));
-        model.addAttribute("criterio10", criterioService.findOne(id_criterio10));
-        model.addAttribute("criterio11", criterioService.findOne(id_criterio11));
-        model.addAttribute("criterio12", criterioService.findOne(id_criterio12));
-        model.addAttribute("criterio13", criterioService.findOne(id_criterio13));
-        model.addAttribute("criterio14", criterioService.findOne(id_criterio14));
-        model.addAttribute("criterio15", criterioService.findOne(id_criterio15));
-        model.addAttribute("criterio16", criterioService.findOne(id_criterio16));
-        model.addAttribute("criterio17", criterioService.findOne(id_criterio17));
-        model.addAttribute("criterio18", criterioService.findOne(id_criterio18));
-        model.addAttribute("criterio19", criterioService.findOne(id_criterio19));
-        model.addAttribute("criterio20", criterioService.findOne(id_criterio20));
-        model.addAttribute("criterio21", criterioService.findOne(id_criterio21));
-        model.addAttribute("criterio22", criterioService.findOne(id_criterio22));
-        model.addAttribute("criterio23", criterioService.findOne(id_criterio23));
-        model.addAttribute("criterio24", criterioService.findOne(id_criterio24));
-        model.addAttribute("criterio25", criterioService.findOne(id_criterio25));
-        model.addAttribute("criterio26", criterioService.findOne(id_criterio26));
-        model.addAttribute("criterio27", criterioService.findOne(id_criterio27));
-        model.addAttribute("criterio28", criterioService.findOne(id_criterio28));
-        model.addAttribute("criterio29", criterioService.findOne(id_criterio29));
-        model.addAttribute("criterio30", criterioService.findOne(id_criterio30));
-        model.addAttribute("criterio31", criterioService.findOne(id_criterio31));
-        model.addAttribute("criterio32", criterioService.findOne(id_criterio32));
-        model.addAttribute("criterio33", criterioService.findOne(id_criterio33));
-        model.addAttribute("criterio34", criterioService.findOne(id_criterio34));
-        model.addAttribute("criterio35", criterioService.findOne(id_criterio35));
-        model.addAttribute("criterio36", criterioService.findOne(id_criterio36));
-        model.addAttribute("criterio37", criterioService.findOne(id_criterio37));
-        model.addAttribute("criterio38", criterioService.findOne(id_criterio38));
-        model.addAttribute("criterio39", criterioService.findOne(id_criterio39));
-        model.addAttribute("criterio40", criterioService.findOne(id_criterio40));
-        model.addAttribute("criterio41", criterioService.findOne(id_criterio41));
-        model.addAttribute("criterio42", criterioService.findOne(id_criterio42));
-
+    
+        Long[] idsCriterios = new Long[] {
+            1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L, 11L, 12L, 13L, 14L, 15L, 16L, 17L, 18L, 19L, 20L,
+            21L, 22L, 23L, 24L, 25L, 26L, 27L, 28L, 29L, 30L, 31L, 32L, 33L, 34L, 35L, 36L, 37L, 38L, 39L, 40L, 41L, 42L
+        };
+    
+        for (int i = 0; i < idsCriterios.length; i++) {
+            model.addAttribute("criterio" + (i + 1), criterioService.findOne(idsCriterios[i]));
+        }
+    
         return "evaluacion/form-evaluacion";
-
+        }else{
+            return "redirect:LoginR";
+        }
+        
     }
 
     // Boton para Guardar Documento
@@ -195,6 +127,10 @@ public class EvaluacionController {
         List<Evaluacion> listEvaluacion = evaluacionService.findByProyectoId(idProyecto);
         int puntajeTotal = 0;
         int cantidadJurados = listjurado.size();
+
+        if (evaluacionDao.validacionEvaluacionJurado(idProyecto, jurado.getId_jurado()).size() >= 1) {
+            return "redirect:/ProyectosEvaluacionR?alert=false";
+        }
 
         for (Long id : id_criterio) {
             Criterio criterio = criterioService.findOne(id);
@@ -241,7 +177,7 @@ public class EvaluacionController {
         }
         redirectAttrs.addFlashAttribute("mensaje", "Proyecto Evaluado Correctamente");
                 
-
+        
         return "redirect:/ProyectosEvaluacionR?alert=true";
     }
 
--- proyecto/src/main/java/com/example/proyecto/Models/Dao/IEvaluacionDao.java
+++ proyecto/src/main/java/com/example/proyecto/Models/Dao/IEvaluacionDao.java
@@ -17,4 +17,18 @@ public interface IEvaluacionDao extends CrudRepository<Evaluacion, Long>{
 
     @Query("SELECT e FROM Evaluacion e JOIN e.proyectos p WHERE p.id = :proyectoId")
     List<Evaluacion> findByProyectoId(@Param("proyectoId") Long proyectoId);
+
+    // @Query(value = "SELECT e FROM evaluacion_proyecto ep \n" + //
+    //             "LEFT JOIN evaluacion e  ON e.id_evaluacion = ep.id_evaluacion \n" + //
+    //             "LEFT JOIN  proyecto p ON p.id_proyecto = ep.id_proyecto \n" + //
+    //             "LEFT JOIN  jurado j ON j.id_jurado  = e.id_jurado \n" + //
+    //             "WHERE p.id_proyecto = ?1 AND j.id_jurado = ?2",nativeQuery = true)
+    // List<Evaluacion> validacionEvaluacionJurado(Long id_proyecto , Long id_jurado);
+
+    @Query(value = "SELECT e.* FROM evaluacion_proyecto ep " +
+               "LEFT JOIN evaluacion e ON e.id_evaluacion = ep.id_evaluacion " +
+               "LEFT JOIN proyecto p ON p.id_proyecto = ep.id_proyecto " +
+               "LEFT JOIN jurado j ON j.id_jurado = e.id_jurado " +
+               "WHERE p.id_proyecto = ?1 AND j.id_jurado = ?2", nativeQuery = true)
+    List<Evaluacion> validacionEvaluacionJurado(Long id_proyecto, Long id_jurado);
 }
