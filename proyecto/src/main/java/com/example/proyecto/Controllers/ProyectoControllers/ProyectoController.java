57ae5a95931cbdf4b0a02abde522a90c692bbd92
jhowillneytan <jhowillneytan@gmail.com>
2024-11-19 10:05:56 -0400

    AS

--- proyecto/src/main/java/com/example/proyecto/Controllers/ProyectoControllers/ProyectoController.java
+++ proyecto/src/main/java/com/example/proyecto/Controllers/ProyectoControllers/ProyectoController.java
@@ -90,7 +90,7 @@ public class ProyectoController {
     public String ProyectoForm(Model model, @PathVariable(name = "id_tipoProyecto") Long id_tipoProyecto) {
 
         // Definir un array con los nombres de los fragmentos
-        String[] fragments = { "card_body1", "card_body2", "card_body3", "card_body4", "card_body5", "card_body6", "card_body7", "card_body8" };
+        String[] fragments = { "card_body1", "card_body2", "card_body3", "card_body4", "card_body5", "card_body6", "card_body7", "card_body8", "card_body9", "card_body10", "card_body11" };
 
         // Verificar si el id_tipoProyecto es válido
         if (id_tipoProyecto >= 1 && id_tipoProyecto <= fragments.length) {
@@ -112,7 +112,7 @@ public class ProyectoController {
     @GetMapping("/lista_proyectos/{id_tipoProyecto}")
     public String lista_proyectos(@PathVariable(name = "id_tipoProyecto") Long id_tipoProyecto, Model model) {
         // Definir un array con los nombres de los fragmentos
-        String[] fragments = { "table1", "table2", "table3", "table4", "table5", "table6", "table7", "table8" };
+        String[] fragments = { "table1", "table2", "table3", "table4", "table5", "table6", "table7", "table8", "table9", "table10", "table11" };
 
         // Verificar si el id_tipoProyecto es válido
         if (id_tipoProyecto >= 1 && id_tipoProyecto <= fragments.length) {
